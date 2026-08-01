e8dde7d6a8eb88205447e1fbd38c6a25dd34f402
lanand <anand.lalit@gmail.com>
2026-07-31 22:09:52 -0700

    excluding pycache and pyc from view, with some updates to the runner

--- src/loganalyzer/runner.py
+++ src/loganalyzer/runner.py
@@ -4,20 +4,26 @@ import logging
 
 #setup logger
 logging.basicConfig(level=logging.INFO)
-logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 #setup sys path
 from utils import utilities
 utilities.add_root_to_syspath()
 
-#generate test log data
+#generate test log data, in deployed env system will be fed with the live logs data
 from data import data_generator
 data_generator.generate_data_file(num_lines=10000)
 
 
 #initialize database and connections
-
+from loganalyzer.repository.databasemgr import DatabaseManager
+database_handler = DatabaseManager()
+database_handler.connect()
+database_handler.init_database()
+database_handler.execute_sql("INSERT INTO logs (ip, http_method, http_api_path, http_response_code) VALUES ('127.0.0.1', 'GET', '/api/users', '200')")
+rs = database_handler.execute_sql("select * from logs")
+log.info(f"the stored ip {rs[0][1]}")
 
 #parse generated data and write to db
 
